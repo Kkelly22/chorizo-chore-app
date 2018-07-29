e854596e8ff5f0732f88966560ec83e08bac793d
Kkelly22 <kk504408@gmail.com>
2018-07-29 19:42:51 -0400

    updated config routes with home page and session info. raked db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :chores
   resources :households
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
