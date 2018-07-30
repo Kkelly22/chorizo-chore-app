4b7d672cb51bdc5d27357f70918dbd2efea39778
Kkelly22 <kk504408@gmail.com>
2018-07-29 20:20:15 -0400

    gave a user a secure password and validated the presence of key fields with error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
 	belongs_to :holdhold
 	has_many :chores
 	has_many :completions
+
+	has_secure_password
+	
+	validates :name, 
+          :presence => {:message => "Name can't be blank." },
+          :uniqueness => {:message => "Title already exists."}
+
+    validates :email, 
+          :presence => {:message => "Email can't be blank." },
+          :uniqueness => {:message => "Email already exists."}
+
+    validates :password, 
+          :presence => {:message => "Password can't be blank." }
 end
