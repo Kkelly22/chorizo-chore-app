fd7f76b5d07ca5174b51567455be62aa63779df4
Kkelly22 <kk504408@gmail.com>
2018-07-29 20:19:44 -0400

    added flow of creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+	def show
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+		if @user && @user.valid?
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password_digest, :image, :roommate_rating, :points)
+	end
+
+
+
 end
