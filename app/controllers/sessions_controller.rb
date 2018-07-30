2e2a02c7672b9f4d6c89bb7de59951c6bcb9668b
Kkelly22 <kk504408@gmail.com>
2018-07-29 20:21:23 -0400

    added basic new session logic to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
 
+	def new
+		@user = User.find_by(id: params[:id])
+		if @user && @user.authenticate
+			session[:user_id] = @user.id
+		else
+			render :new
+		end
+	end
+
+	def create
+	end
+
+	def destroy
+	end
 	
+
+
 end
