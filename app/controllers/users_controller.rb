f2f416f0158062887136a656604fdf50f3d746a3
Kkelly22 <kk504408@gmail.com>
2018-07-31 08:43:57 -0400

    added household name to New User form, added errors to views, fixed up show views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:name, :email, :password, :image, :roommate_rating, :points)
+		params.require(:user).permit(:name, :email, :password, :image, :roommate_rating, :points, :household_name)
 	end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	belongs_to :holdhold
+	belongs_to :household
 	has_many :chores
 	has_many :completions
 
@@ -15,4 +15,14 @@ class User < ApplicationRecord
 
     validates :password, 
           :presence => {:message => "Password can't be blank." }
+
+    def household_name=(name)
+      self.household = Household.find_or_create_by(name: name)
+    end
+
+    def household_name
+      self.household ? self.household.name : nil
+    end
+
+
 end
