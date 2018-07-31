4aaa4ac58cd29f4c578e3d82726437c84f1b0af7
Kkelly22 <kk504408@gmail.com>
2018-07-31 15:08:24 -0400

    added custom getters/setters for household/user relationships

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,4 +3,20 @@ class Chore < ApplicationRecord
 	belongs_to :user
 	has_many :completions
 
+	def household_name=(name)
+      self.household = Household.find_or_create_by(name: name)
+    end
+
+    def household_name
+      self.household ? self.household.name : nil
+    end
+
+   	def user_name=(name)
+      self.user = User.find_or_create_by(name: name)
+    end
+
+    def user_name
+      self.user ? self.user.name : nil
+    end
+
 end
