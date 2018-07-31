2343c303c9a604a8caa963a58215038da8f5b0f4
Kkelly22 <kk504408@gmail.com>
2018-07-30 22:11:53 -0400

    moved logic from new to create, added destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,22 @@
 class SessionsController < ApplicationController
 
 	def new
-		@user = User.find_by(id: params[:id])
-		if @user && @user.authenticate
+		@users = User.all
+	end
+
+	def create
+		@user = User.find_by(email: params[:email])
+		if @user && @user.authenticate(params[:password])
 			session[:user_id] = @user.id
+			redirect_to user_path(@user)
 		else
 			render :new
 		end
 	end
 
-	def create
-	end
-
 	def destroy
+		session.delete :user_id
+		redirect_to root_path
 	end
 	
 
