d2e808f95d756b558acba22378283e388ae5eb14
Kkelly22 <kk504408@gmail.com>
2018-07-31 15:09:15 -0400

    added the basical controller actions for Chores

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,9 +1,49 @@
 class ChoresController < ApplicationController
 
+	def index
+		@chores = Chore.all
+	end
+
 	def new
+		@chore = Chore.new
 	end
 
 	def create
+		@chore = Chore.create(chore_params)
+		if @chore
+			redirect_to chore_path(@chore)
+		else
+			render :new
+		end
 	end
-	
+
+	def show
+		@chore = Chore.find_by(id: params[:id])
+	end
+
+	def edit
+		@chore = Chore.find_by(id: params[:id])
+	end
+
+	def update
+		@chore = Chore.update(chore_params)
+		if @chore
+			redirect_to chore_path(@chore)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@chore = Chore.find_by(id: params[:id])
+		@chore.destroy
+		redirect_to chores_path
+	end
+
+	private
+
+	def chore_params
+		params.require(:chore).permit(:description, :due_date, :point_value, :household_name, :user_name)
+	end
+
 end
