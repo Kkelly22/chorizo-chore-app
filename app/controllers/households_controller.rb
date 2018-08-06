e44defcd41c4decac12677bfa71a963dc81e4f80
Kkelly22 <kk504408@gmail.com>
2018-08-06 18:24:03 -0400

    added the household MVC flow

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -1,2 +1,50 @@
 class HouseholdsController < ApplicationController
+	layout "users"
+
+	def index
+		@households = Household.all
+	end
+
+	def new
+		@household = Household.new
+	end
+
+	def create
+		@household = Household.create(household_params)
+		if @household
+			redirect_to household_path(@household)
+		else
+			redirect_to new_household_path
+		end
+	end
+
+	def show
+		@household = Household.find_by(id: params[:id])
+	end
+
+	def edit
+		@household = Household.find_by(id: params[:id])
+	end
+
+	def update
+		@household = Household.update(household_params)
+		if @household
+			redirect_to household_path(@household)
+		else
+			redirect_to edit_household_path
+		end
+	end
+
+	def destroy
+		@household = Household.find_by(id: params[:id])
+		@household.destroy
+		redirect_to households_path
+	end
+
+	private
+
+	def household_params
+		params.require(:household).permit(:name, :user_name)
+	end
+
 end
--- app/models/household.rb
+++ app/models/household.rb
@@ -2,4 +2,12 @@ class Household < ApplicationRecord
 	has_many :users
 	has_many :chores
 	
+	def user_name=(name)
+    	self.user = User.find_or_create_by(name: name)
+ 	end
+
+  	def user_name
+    	self.user ? self.user.name : nil
+  	end
+
 end
