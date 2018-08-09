5bea97073d2194e617028943b4eb50564d94a12e
Kkelly22 <kk504408@gmail.com>
2018-08-09 19:32:21 -0400

    added initial MVC for the assignment of chores through completions

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,34 +1,26 @@
 class CompletionsController < ApplicationController
 	layout "users"
 
-	def index
-		if params[:user_id]
-      		@completions = User.find(params[:user_id]).completions
-      		@completion_index_header = "Your Completions"
-    	else
-      		@completions = Chore.all
-      		@completion_index_header = "All Completions"
-    	end
-	end
-
 	def new
+		@chore = Chore.find_by(params[:chore_id])
 		@completion = Completion.new
 	end
 
 	def create
 		@completion = Completion.create(completion_params)
 		if @completion
-			redirect_to user_chores_path(current_user)
+			redirect_to root_path
 		else
-			redirect_to user_chore_path(current_user, params[:chore_id])
+			redirect_to new_completion_path
 		end
 	end
+
 
 
 	private
 
 	def completion_params
-		params.require(:completion).permit(:chore_name, :user_name, :due_date)
+		params.require(:completion).permit(:chore_id, :user_name, :due_date)
 	end
 
 end
--- app/models/completion.rb
+++ app/models/completion.rb
@@ -1,5 +1,13 @@
 class Completion < ApplicationRecord
 	belongs_to :user
 	belongs_to :chore
+
+	def user_name=(name)
+      self.user = User.find_or_create_by(name: name)
+    end
+
+    def user_name
+      self.user ? self.user.name : nil
+    end
 	
 end
