59e7c543726ffcf50311e1608cac771d6cd822f7
Kkelly22 <kk504408@gmail.com>
2018-08-09 20:58:19 -0400

    added view and complete chores functionality

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,10 @@
 class CompletionsController < ApplicationController
 	layout "users"
 
+	def index
+		@completions = current_user.completions.all
+	end
+
 	def new
 		@chore = Chore.find_by(params[:chore_id])
 		@completion = Completion.new
@@ -13,6 +17,19 @@ class CompletionsController < ApplicationController
 		else
 			redirect_to new_completion_path
 		end
+	end
+
+	def complete
+		@completion = Completion.find_by(id:params[:completion_id])
+		if @completion
+			@completion.completed = true
+			@completion.save
+
+			@user = @completion.user
+			@user.points_earned += @completion.chore.point_value
+			@user.save
+		end
+		redirect_to root_path
 	end
 
 
