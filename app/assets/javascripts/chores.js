a5d62de0d4067a509022353670d25a4e287cb843
Kkelly22 <kk504408@gmail.com>
2019-03-12 13:11:02 -0400

    added id of myTab to the navbar, added event listeners to several tabs with a place holder alert

--- app/assets/javascripts/chores.js
+++ app/assets/javascripts/chores.js
@@ -1,1 +1,21 @@
-//alert("Chores Working!");
+$(document).ready(function() {
+   attachListeners();
+});
+
+
+function attachListeners () {
+ 	$('#myTab li:nth-child(2) a').on('click', function (e) {
+  		e.preventDefault()
+  		alert("My Chores clicked!");
+	})
+
+	$('#myTab li:nth-child(3) a').on('click', function (e) {
+  		e.preventDefault()
+  		alert("All Chores clicked!");
+	})
+
+	$('#myTab li:nth-child(4) a').on('click', function (e) {
+  		e.preventDefault()
+  		alert("Manage Households clicked!");
+	})
+}
