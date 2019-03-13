674d42bc6418967108a9299d8e328ade332e638d
Kkelly22 <kk504408@gmail.com>
2019-03-12 20:56:58 -0400

    re-coded the click event, added a div for the additional info in the view file

--- app/assets/javascripts/chores.js
+++ app/assets/javascripts/chores.js
@@ -1,45 +1,27 @@
 $(document).ready(function() {
-   attachListeners();
-
-   
+	attachListeners();   
 });
 
 
 function Chore(attributes) {
 	this.description = attributes.description;
 	this.point_value = attributes.point_value;
-	this.household_name = attributes.household_name;
+	this.household_id = attributes.household_id;
 	this.id = attributes.id;
-	debugger;
 }
-
-
-$(function() {
-	Chore.templateSource = $("#chores-template").html();
-	Chore.template = Handlebars.compile(Chore.templateSource);
-})
-
-
-
-Chore.prototype.renderIndex = function () {
-	debugger;
-	return Chore.template(this)
-}
-
-
 
 function attachListeners () {
-	$('#myTab li:nth-child(3) a').on('click', function (e) {
-  		e.preventDefault();
-    	$.get("/chores" + ".json", function(data) {
-    		var chore = new Chore(data[0]);
-    		debugger;
-    		var choreIndex = chore.renderIndex();
-    		debugger;
-    		$("ul.chore-list").append(choreIndex);
-    	});
-	})
+	$('.js-more').on('click', function (e) {
+ 		e.preventDefault();
+ 		var id = $(this).data("id");
 
+   		$.get("/chores/" + id + ".json", function(data) {
+   			var chore = new Chore(data);
+	   		var descriptionText = "<p>Point Value: " + chore.point_value + "</p>"
+   			$("#chore-" + id).html(descriptionText);
+   		});
+	})
 }
+
 
 
