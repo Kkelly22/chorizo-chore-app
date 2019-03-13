74b7ab88c529ffc15f942a977a95eae02d739a26
Kkelly22 <kk504408@gmail.com>
2019-03-12 21:47:55 -0400

    added household name to serializer attributes to mimic the model

--- app/serializers/chore_serializer.rb
+++ app/serializers/chore_serializer.rb
@@ -1,4 +1,4 @@
 class ChoreSerializer < ActiveModel::Serializer
-  attributes :id, :description, :point_value
+  attributes :id, :description, :point_value, :household_name
   belongs_to :household
 end
