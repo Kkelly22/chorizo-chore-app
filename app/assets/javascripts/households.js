db6d6e19c91e9ae557686ce6e12496ddbea6bfd6
Kkelly22 <kk504408@gmail.com>
2019-03-13 18:06:48 -0400

    removed the previous view of households, added a view all households link, and added AJAX to render the household links dynamically

--- app/assets/javascripts/households.js
+++ app/assets/javascripts/households.js
@@ -16,6 +16,10 @@ function formatHousehold(householdChore) {
 	"Belongs to Household: " + householdChore.household_name + "</p>"
 }
 
+function formatHouse(household) {
+	return '<li><a href="/households/' + household.id + '"">' + household.name + '</a><br>'
+}
+
 function attachHouseholdListeners () {
 	$('.js-chores').on('click', function (e) {
  		e.preventDefault();
@@ -23,8 +27,22 @@ function attachHouseholdListeners () {
 
    		$.get("/households/" + id + ".json", function(data) {
    			let household = new Household(data);
+   			$("#household-" + id + "-chores").html("");
    			for (let i = 0; i < household.chores.length; i++) {
 				$("#household-" + id + "-chores").append(formatHousehold(household.chores[i]));
+			}
+   		});
+	})
+
+
+	$('#js-households').on('click', function (e) {
+  		e.preventDefault()
+  		$.get("/households" +  ".json", function(data) {
+  			debugger;
+  			$("#households").html("");
+  			for (let i = 0; i < data.length; i++) {
+  				let household = new Household(data[i]);
+				$("#households").append(formatHouse(household));
 			}
    		});
 	})
